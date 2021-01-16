b34e5c648db55d66a7a707e9bc1b6587212a51de
Ajay35 Kumar <ajay35.kumar@197NODMB300033.local>
2021-01-16 18:54:32 +0530

    removed junit xml report from plugins

--- src/test/java/runners/CustomTestRunner.java
+++ src/test/java/runners/CustomTestRunner.java
@@ -14,7 +14,6 @@ import java.util.Iterator;
         plugin = {  "pretty",
                     "html:test-output",
                     "json:target/cucumber-json-report.json",
-//                    "junit:junit_xml/cucumber.xml",
                     "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         }
 )
